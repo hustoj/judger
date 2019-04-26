3618589626f995ec9ab05e557c2d20f4b4a6c9dc
He Jun <freefcw@gmail.com>
2019-04-27 01:24:31 +0800

    fix(compiler): fix a logger typo

--- judge/compiler/compiler.py
+++ judge/compiler/compiler.py
@@ -54,15 +54,14 @@ class Compiler2(object):
 
     def _compile(self):
         args = self.language_type.full_compile_command()
-        logger = logger()
-        logger.debug('Compile task use {args}'.format(args=args))
+        logger().debug('Compile task use {args}'.format(args=args))
         p = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         signal(SIGALRM, alarm_handler)
         alarm(MAX_COMPILE_TIME)
         try:
             (stdoutdata, stderrdata) = p.communicate()  # python 3.3 add timeout
             if stderrdata or stdoutdata:
-                logger.warning("Compile alert: out => %s, err => %s", stdoutdata, stderrdata)
+                logger().warning("Compile alert: out => %s, err => %s", stdoutdata, stderrdata)
             alarm(0)
             if p.returncode != 0:
                 raise CompileException(stderrdata)
