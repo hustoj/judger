da9ea657c500c871d5f3f2d51873019620f9af77
He Jun <freefcw@gmail.com>
2022-08-10 10:22:13 +0800

    optimize data store

--- judge/data/store.py
+++ judge/data/store.py
@@ -12,6 +12,9 @@ LOGGER = logging.getLogger(__name__)
 class LocalCache(object):
     def __init__(self, path) -> None:
         super().__init__()
+
+        if not os.path.exists(path):
+            os.makedirs(path)
         self.path = path
 
     def cached(self, pid):
@@ -19,13 +22,12 @@ class LocalCache(object):
             return True
         return False
 
-    def save_data(self, pid, content):
-        # type: (int, bytes) -> None
+    def save_data(self, pid: int, content: bytes) -> None:
         path = self._get_data_path(pid)
         LOGGER.info('write {path} data'.format(path=path))
         write_file(path, content.decode())
 
-    def get_data(self, pid):
+    def get_data(self, pid: int):
         path = self._get_data_path(pid)
         LOGGER.info('get data of %d, %s', pid, path)
         return get_file_content(path)
@@ -40,31 +42,26 @@ class DataStore(object):
     remote: WebApi
     cache: LocalCache
 
-    def set_cache(self, cache):
+    def set_cache(self, cache: LocalCache):
         self.cache = cache
 
     def set_remote(self, api):
         self.remote = api
 
-    def get_data(self, pid):
-        """
-        :param pid:
-        :return: judge.data.CaseManager
-        """
+    def get_data(self, pid: int) -> CaseManager:
         if self.is_cached(pid):
             return CaseManager(self.cache.get_data(pid))
 
         LOGGER.info('Data of {pid} is not cached, will fetch from remote'.format(pid=pid))
         return CaseManager(self.fetch_data(pid))
 
-    def fetch_data(self, pid):
+    def fetch_data(self, pid: int):
         response = self.remote.get_data(pid)
         self.write_data(pid, response)
 
         return response.to_data()
 
-    def write_data(self, pid, response):
-        # type: (int, DataResponse) -> None
+    def write_data(self, pid: int, response: DataResponse) -> None :
         if self.cache:
             self.cache.save_data(pid, response.to_data())
 
